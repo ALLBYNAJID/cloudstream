b0ac10bbef9e99dadb48a49f860258ef0b162ef7
PERFECTALL <najidpubglover12@gmail.com>
2025-05-18 12:47:08 +0600

    Update build.gradle.kts

--- Turkish/build.gradle.kts
+++ Turkish/build.gradle.kts
@@ -22,5 +22,5 @@ cloudstream {
         "Movie",
     )
 
-    iconUrl = "https://www.google.com/s2/favicons?domain=yoturkish.com&sz=%size%"
+    iconUrl = "https://www.google.com/s2/favicons?domain=turkish123.com&sz=%size%"
 }
