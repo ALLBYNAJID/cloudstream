ba2a3041fbe31f48b2c91ac22a661f058a72e305
PERFECTALL <najidpubglover12@gmail.com>
2025-05-18 22:18:15 +0600

    Update build.gradle.kts

--- StreamPlay/build.gradle.kts
+++ StreamPlay/build.gradle.kts
@@ -77,6 +77,6 @@ cloudstream {
 }
 
 dependencies {
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.browser:browser:1.8.0")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.15.3")
 }
