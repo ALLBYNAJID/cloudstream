2b9f90317fba73d9b76dcd7028afddd27c8e452b
PERFECTALL <najidpubglover12@gmail.com>
2025-05-18 17:49:16 +0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,23 +5,25 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 buildscript {
     repositories {
-        google()
         mavenCentral()
+        // Shitpack repo which contains our tools and dependencies
         maven("https://jitpack.io")
+        google()
     }
 
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
+        // Cloudstream gradle plugin which makes everything work and builds plugins
         classpath("com.github.recloudstream:gradle:-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22") // Compatible and stable
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
 }
 
 allprojects {
     repositories {
-        google()
         mavenCentral()
         maven("https://jitpack.io")
+        google()
     }
 }
 
@@ -35,16 +37,17 @@ subprojects {
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
     cloudstream {
+        // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
+        // you can modify it to use other git hosting services, like gitlab
         setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/ALLBYNAJID/cloudstream")
     }
 
     android {
         namespace = "recloudstream"
 
-        compileSdkVersion(35) // FIXED HERE
-
         defaultConfig {
             minSdk = 21
+            compileSdkVersion(35)
             targetSdk = 35
         }
 
@@ -53,9 +56,9 @@ subprojects {
             targetCompatibility = JavaVersion.VERSION_1_8
         }
 
-        tasks.withType<KotlinJvmCompile>().configureEach {
+        tasks.withType<KotlinJvmCompile> {
             compilerOptions {
-                jvmTarget.set(JvmTarget.JVM_1_8)
+                jvmTarget.set(JvmTarget.JVM_1_8) // Required
                 freeCompilerArgs.addAll(
                     "-Xno-call-assertions",
                     "-Xno-param-assertions",
@@ -69,14 +72,19 @@ subprojects {
         val cloudstream by configurations
         val implementation by configurations
 
+        // Stubs for all cloudstream classes
         cloudstream("com.github.recloudstream.cloudstream:pre-release")
-        implementation(kotlin("stdlib"))
-        implementation("com.github.Blatzar:NiceHttp:0.4.12")
-        implementation("org.jsoup:jsoup:1.18.3")
+
+        // These dependencies can include any of those which are added by the app,
+        // but you don't need to include any of them if you don't need them.
+        // https://github.com/recloudstream/cloudstream/blob/master/app/build.gradle.kts
+        implementation(kotlin("stdlib")) // Adds Standard Kotlin Features
+        implementation("com.github.Blatzar:NiceHttp:0.4.12") // HTTP Lib
+        implementation("org.jsoup:jsoup:1.18.3") // HTML Parser
         implementation("com.github.recloudstream.cloudstream:library-jvm:pre-release")
     }
 }
 
-tasks.register<Delete>("clean") {
+task<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
