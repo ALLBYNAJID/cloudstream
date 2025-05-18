76b6cedab5353fc96b94ad568acea1d2f22d7a86
PERFECTALL <najidpubglover12@gmail.com>
2025-05-18 15:47:51 +0600

    Update StreamPlayPlugin.kt

--- StreamPlay/src/main/kotlin/com/najid/StreamPlayPlugin.kt
+++ StreamPlay/src/main/kotlin/com/najid/StreamPlayPlugin.kt
@@ -2,7 +2,7 @@ package com.najid
 
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
-import com.phisher98.settings.SettingsFragment
+import com.najid.settings.SettingsFragment
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.extractors.DoodYtExtractor
 import com.lagradost.cloudstream3.extractors.FileMoon
