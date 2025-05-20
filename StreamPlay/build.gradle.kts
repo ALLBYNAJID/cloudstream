8d1c8275c8ffbeaf5512c57a5b9fb30f8681eb26
PERFECTALL <najidpubglover12@gmail.com>
2025-05-21 01:17:34 +0600

    Update build.gradle.kts

--- StreamPlay/build.gradle.kts
+++ StreamPlay/build.gradle.kts
@@ -50,7 +50,7 @@ cloudstream {
     // All of these properties are optional, you can safely remove them
 
      description = "#1 best extention based on MultiAPI"
-     authors = listOf("najid", "Hexated","salman731")
+     authors = listOf("Najid")
 
     /**
      * Status int as the following:
