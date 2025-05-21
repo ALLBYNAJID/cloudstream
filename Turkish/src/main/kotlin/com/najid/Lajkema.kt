a6f7ad01b0ce850c6b6990c74fe9fba79597d3bb
PERFECTALL <najidpubglover12@gmail.com>
2025-05-21 23:39:34 +0600

    Delete Turkish/src/main/kotlin/com/najid/Lajkema.kt

--- Turkish/src/main/kotlin/com/najid/Lajkema.kt
+++
@@ -1,8 +0,0 @@
-package com.najid
-
-import com.lagradost.cloudstream3.extractors.XStreamCdn
-
-class Lajkema : XStreamCdn() {
-    override val name: String = "Lajkema"
-    override val mainUrl: String = "https://lajkema.com"
-}
