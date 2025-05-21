819a78c195f7124293dd1170a01e428a3963c3c7
PERFECTALL <najidpubglover12@gmail.com>
2025-05-21 23:39:56 +0600

    Update TurkishPlugin.kt

--- Turkish/src/main/kotlin/com/najid/TurkishPlugin.kt
+++ Turkish/src/main/kotlin/com/najid/TurkishPlugin.kt
@@ -10,6 +10,5 @@ class TurkishPlugin: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(Turkish())
-        registerExtractorAPI(Lajkema())
     }
 }
